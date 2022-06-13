4765fa06987be66010589442e83ea38ff30aa441
Sandrine Le Diraison <didine.perla@gmail.com>
2022-06-13 10:10:17 +0200

    "Modif"

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
+//Nom de l'utilisateur
+/*var prenom = prompt("Comment vous appelez-vous ?");
 
+var salutation = "bonjour " + prenom;
+
+document.body.innerHTML += "<h2>" + salutation + "</h2>";
+
+console.log(salutation);*/
+
+
+//Code de Zoé
+function askName() {
+    let person = prompt("Name ?")
+    var hiName = ("&#9995 bonjour " + person)
+    document.body.innerHTML += '<h2>' + hiName + '<h2>';
+}
+askName();
+
+
+
+//L'âge de l'utilisateur
+var year = prompt("En quelle anné êtes-vous né ?");
+
+function askBirthYear(date) {
+    var diff = Date.now() - date.getTime();
+    var age = new Date(diff);
+    return Math.abs(age.getUTCFullYear() - 1970);
+}
+alert(askBirthYear(new Date(year))); 
